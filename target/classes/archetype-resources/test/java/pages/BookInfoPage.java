214bf4e3f3543d1bc948ccc207c24148e7899f93
Sundaresan <sundakr1@in.ibm.com>
2019-10-24 17:27:56 +0530

    fixed untracked files

--- target/classes/archetype-resources/test/java/pages/BookInfoPage.java
+++
@@ -1,8 +0,0 @@
-package pages;
-
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("http://35.224.212.34/productpage")
-class BookInfoPage extends PageObject {}
